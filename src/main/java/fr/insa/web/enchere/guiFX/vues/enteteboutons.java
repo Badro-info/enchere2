5da9f10d66ee7cf02129b4da5918be5c3f62b9d4
badro <badro@172.29.15.175>
2022-11-15 00:22:13 +0100

    a la maison jai continue de inscription

---
+++ src/main/java/fr/insa/web/enchere/guiFX/vues/enteteboutons.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.web.enchere.guiFX.vues;
+
+import javafx.scene.layout.HBox;
+
+/**
+ *
+ * @author badro
+ */
+public class enteteboutons extends HBox {
+//    je vais mettre les boutons proposer, rechercher et logout
+}
---
+++ src/main/java/fr/insa/web/enchere/guiFX/vues/proposer.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.web.enchere.guiFX.vues;
+
+import fr.insa.badreddine.enchere.Enchere;
+import fr.insa.badreddine.enchere.guifx.VuePrincipale;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+/**
+ *
+ * @author badro
+ */
+public class proposer extends GridPane{
+//    private VuePrincipale main;
+////caracteristique pour pouvoir s'inscrire
+//    private TextField tfdescription;
+//    private TextField tftitre;
+//    private TextField tfcategorie;
+//    private TextField tfproposerpar;
+//
+//    private Timestamp tfdatefin;
+//    private Timestamp tfdebut;
+//    private TextField tfprixbase;
+////    boutton pour valider l'inscription
+//    private Button bOK;
+////    private Button bcategorie;
+//    
+//    public proposer(VuePrincipale main) {
+//        this.main = main;
+//        this.add(new Label("Categorie"), 0, 0);
+//        this.tfcategorie = new TextField();
+//        this.add(this.tfcategorie, 1, 0);
+//        this.add(new Label("proposer Par"), 0, 1);
+//        this.tfproposerpar = new TextField();
+//        this.add(this.tfproposerpar, 1, 1);
+//        this.add(new Label("Description"), 0, 2);
+//        this.tfdescription = new TextField();
+//        this.add(this.tfdescription, 1, 2);
+//        this.add(new Label("Titre"), 0, 3);
+//        this.tftitre = new TextField();
+//        this.add(this.tftitre, 1, 3);
+//        this.add(new Label("date de fin"), 0, 4);
+//        this.tfdatefin = new Timestamp();
+//        this.add(this.tfdatefin, 1, 4);
+//        this.add(new Label("datedebut"), 0, 5);
+//        this.tfdatedebut = new Timestamp();
+//        this.add(this.tfdatedebut, 1, 5);
+//        this.add(new Label("Prix Base"), 0, 6);
+//        this.tfprixbase = new TextField();
+//        this.add(this.tfprixbase, 1, 6);
+//        this.bOK = new Button("OK");
+//        this.add(bOK, 0, 7, 2, 1);
+//        this.bOK.setOnAction((t) -> {
+//            //je prend les donnees entrees par l'utilisateur
+////            String descriptionCourte = tfdescriptioncourte.getText();
+////            String descriptiondetaille = tfdescriptiondetaille.getText();
+//            Timestamp datefin = tfdatefin.getTime();
+//            int prixbase = tfprixbase.getText();
+//            Timestamp datedebut = tfdebut.getTime();
+//            String Titre = tftitre.getText();
+//            String Description = tfdescription.getText();
+//            int Categorie = tfcategorie.getText();
+//            int ProposerPar = tfproposerpar.getText();
+//            try {
+////                je me connecte a la base de donnee pour pouvoir les souvegarder ladans tout en faisant des exceptions
+//                Enchere.createObjet(this.main.getConnectBdD(), Titre, Description, datedebut, datefin, prixbase, Categorie, ProposerPar);
+//            } catch (SQLException ex) {
+//                Alert alert = new Alert(Alert.AlertType.ERROR);
+//                alert.setHeaderText("problem :");
+//                alert.setContentText(ex.getLocalizedMessage());
+//                alert.showAndWait();
+//            }
+//        });
+//    }
+    
+}
